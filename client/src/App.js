0916e93f0bc51add3bb17301feef5dd1ced94132
Christopher Hernandez <cerberean661@gmail.com>
2019-09-06 08:56:02 -0700

    setting up components, render is incomplete

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("first render (mounting)")
+    fetch('http://localhost:5000/api/players')
+    .then(res => res.json())
+    .then(data => this.setState({ players:data }));
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        < />
+      </div>
+    );
+  }
+
 }
 
 export default App;
